d16ddcb47492c31502de5782fed610600c7d55b3
Alexandre Nardes <alexandrenardes00@gmail.com>
2023-01-19 20:22:43 -0300

    date and save

--- Aula 02/vscode/script.js
+++ Aula 02/vscode/script.js
@@ -1,12 +1,40 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+    const today  = new Date().toLocaleDateString('pt-br').slice(0, -5)
+
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert("Dia já incluso")
+        return
+    }
+    alert("Adicionado com Sucesso!")
+    nlwSetup.addDay(today)
+}
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+
+nlwSetup.setData(data)
+
+nlwSetup.load() 
+/* const data = {
     run: ['01-01','01-02','01-06','01-07','01-08'],
     programming: ['01-03','01-06','01-07','01-08'],
     book: ['01-02','01-06','01-07','01-08','01-09','01-10','01-11']
 }
 
+
 nlwSetup.setData(data)
 
-nlwSetup.load()
+nlwSetup.load() */
+
